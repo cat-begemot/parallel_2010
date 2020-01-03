6927b95a5e700c676790ddf62bf0c90a4d30452b
Alex Holenko <oholenko@vector-software.com>
2020-01-03 10:25:10 +0200

    Step2

--- Chapter2/Program.cs
+++ Chapter2/Program.cs
@@ -10,12 +10,207 @@ namespace Chapter2
     {
         public static void Main(string[] args)
         {
-	        PuttingTaskToSleep();
-
+	        LazyTaskExecution();
 
 			Console.WriteLine("Main method complete. Press enter to finish.");
 	        Console.ReadLine();
         }
+
+		private static void LazyTaskExecution()
+		{
+			var taskBody = new Func<string>(() =>
+			{
+				Console.WriteLine("Task body working...");
+				return "Task Result";
+			});
+
+			var lazyData = new Lazy<Task<string>>(() => Task.Factory.StartNew(taskBody));
+			Console.WriteLine($"Calling lazy variable: {lazyData.Value.Result}");
+
+			var lazyData2 = new Lazy<Task<string>>(() => Task.Factory.StartNew(() =>
+			{
+				Console.WriteLine("Task body working...");
+				return "Task Result 2";
+			}));
+
+			Console.WriteLine($"Calling lazy variable: {lazyData2.Value.Result}");
+		}
+
+		private static void CustomEscalationPolicy()
+		{
+			TaskScheduler.UnobservedTaskException += (object sender, UnobservedTaskExceptionEventArgs eventArgs) =>
+			{
+				eventArgs.SetObserved();
+				eventArgs.Exception.Handle(ex =>
+				{
+					Console.WriteLine($"Exception type: {ex.GetType()}");
+					return true;
+				});
+			};
+
+			var task1 = new Task(() => throw new NullReferenceException());
+			Console.WriteLine(task1.Status);
+			var task2 = new Task(() => throw new ArgumentOutOfRangeException());
+
+			task1.Start();
+			Console.WriteLine(task1.Status);
+			task2.Start();
+			Console.WriteLine(task1.Status);
+			while (!task1.IsCompleted || !task2.IsCompleted)
+				Thread.Sleep(500);
+			Console.WriteLine(task1.Status);
+		}
+
+		private static void ReadingTaskProperties()
+		{
+			var tokenSource = new CancellationTokenSource();
+
+			var task1 = new Task(() => throw new NullReferenceException());
+
+			var task2 = new Task(() =>
+			{
+				tokenSource.Token.WaitHandle.WaitOne(-1);
+				throw new OperationCanceledException();
+			}, tokenSource.Token);
+
+			task1.Start();
+			task2.Start();
+			tokenSource.Cancel();
+
+			try
+			{
+				Task.WaitAll(task1, task2);
+			}
+			catch(AggregateException)
+			{ }
+
+			Console.WriteLine($"Task #1 completed: {task1.IsCompleted}");
+			Console.WriteLine($"Task #1 faulted: {task1.IsFaulted}");
+			Console.WriteLine($"Task #1 cancelled: {task1.IsCanceled}");
+			Console.WriteLine(task1.Exception);
+
+			Console.WriteLine();
+
+			Console.WriteLine($"Task #2 completed: {task2.IsCompleted}");
+			Console.WriteLine($"Task #2 faulted: {task2.IsFaulted}");
+			Console.WriteLine($"Task #2 cancelled: {task2.IsCanceled}");
+			Console.WriteLine(task1.Exception);
+		}
+
+		private static void UsingIterativeExceptionHandler()
+		{
+			var tokenSource = new CancellationTokenSource();
+			var token = tokenSource.Token;
+
+			var task1 = new Task(() =>
+			{
+				token.WaitHandle.WaitOne(-1);
+				throw new OperationCanceledException();
+			}, token);
+
+			var task2 = new Task(() => throw new NullReferenceException());
+
+			task1.Start();
+			task2.Start();
+
+			tokenSource.Cancel();
+
+			try
+			{
+				Task.WaitAll(task1, task2);
+			}
+			catch(AggregateException ex)
+			{
+				ex.Handle(inner =>
+				{
+					if (inner is OperationCanceledException)
+					{
+						Console.WriteLine(inner.GetType());
+						return true;
+					}
+
+					if(inner is NullReferenceException)
+					{
+						Console.WriteLine(inner.GetType());
+						return true;
+					}
+
+					Console.WriteLine($"Do not know about this kind of exception: {inner.GetType()}");
+					return false;
+				});
+			}
+		}
+
+		private static void BasicExceptionHandling()
+		{
+			var task1 = new Task(()=>
+			{
+				var exception = new ArgumentOutOfRangeException();
+				exception.Source = "task1";
+				throw exception;
+			});
+
+			var task2 = new Task(()=>
+			{
+				throw new NullReferenceException();
+			});
+
+			var task3 = new Task(()=>
+			{
+				Console.WriteLine("Hello from task3!");
+			});
+
+			task1.Start();
+			task2.Start();
+			task3.Start();
+
+			try
+			{
+				Task.WaitAll(task1, task2, task3);
+			}
+			catch (AggregateException e)
+			{
+				foreach(var inner in e.InnerExceptions)
+					Console.WriteLine($"Exception type: {inner.GetType()} from {inner.Source}");
+			}
+		}
+
+		private static void WaitForSingleTask()
+		{
+			var tokenSource = new CancellationTokenSource();
+			var token = tokenSource.Token;
+
+			var task = CreateTask(token);
+			task.Start();
+			Console.WriteLine("Waiting for task complete");
+			task.Wait();
+			Console.WriteLine("Task completed.");
+
+			task = CreateTask(token);
+			task.Start();
+			Console.WriteLine("Waiting 2 secs for task to complete");
+			bool completed = task.Wait(2000);
+			Console.WriteLine($"Wait ended - task completed: {completed}");
+
+			task = CreateTask(token);
+			task.Start();
+			Console.WriteLine("Wait 2 secs for task to complete.");
+			completed = task.Wait(2000, token);
+			Console.WriteLine($"Wait ended - tasl completed: {completed}, task cancelled: {task.IsCanceled}");
+		}
+
+		private static Task CreateTask(CancellationToken token)
+		{
+			return new Task(() =>
+			{
+				for (int i = 0; i < 5; i++)
+				{
+					token.ThrowIfCancellationRequested();
+					Console.WriteLine($"Task - Int value {i}");
+					token.WaitHandle.WaitOne(1000);
+				}
+			});
+		}
 
         private static void PuttingTaskToSleep()
         {
