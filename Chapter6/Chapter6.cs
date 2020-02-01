c5f73efbeece448ae753658fe4cadc9a40fbfd27
Alex Holenko <cat-begemot@live.com>
2020-02-01 18:41:07 +0200

    Step9

---
+++ Chapter6/Chapter6.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter6
+{
+    class Chapter6
+    {
+        static void Main(string[] args)
+        {
+	        LinqAndPLinqQueriesUsingExtMethods();
+
+	        Console.WriteLine("Press enter to finish...");
+	        Console.ReadLine();
+        }
+
+        private static void LinqAndPLinqQueriesUsingExtMethods()
+		{
+			var sourceData = new int[10];
+			for (int i = 0; i < sourceData.Length; i++)
+				sourceData[i] = i;
+
+			var seqResult = sourceData.Select(item => Math.Pow(item, 2));
+
+			foreach (var result in seqResult)
+				Console.WriteLine($"Sequential result: {result}");
+
+			var parResult = sourceData
+								.AsParallel()
+								.Select(item => Math.Pow(item, 2));
+
+			foreach (var result in parResult)
+				Console.WriteLine($"Parallel result: {result}");
+		}
+
+		private static void LinqAndPLinqQueries()
+        {
+	        var sourceData = new int[10];
+	        for (int i = 0; i < sourceData.Length; i++)
+		        sourceData[i] = i;
+
+	        IEnumerable<double> seqResult =
+		        from item in sourceData
+		        select Math.Pow(item, 2);
+
+			foreach(var result in seqResult)
+				Console.WriteLine($"Sequential result: {result}");
+
+			IEnumerable<double> parResult =
+				from item in sourceData.AsParallel()
+				select Math.Pow(item, 2);
+
+			foreach (var result in parResult)
+				Console.WriteLine($"Parallel result: {result}");
+		}
+    }
+}
